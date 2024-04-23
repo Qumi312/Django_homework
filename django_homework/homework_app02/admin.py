0fbcae8eb745b83ba55eb654c99b515cf675a733
qumi <bahz1997@gmail.com>
2024-04-23 13:13:07 +0400

    Выполнено дз 5

--- django_homework/homework_app02/admin.py
+++ django_homework/homework_app02/admin.py
@@ -1,3 +1,86 @@
 from django.contrib import admin
-
+from .models import Client, Order, Product
 # Register your models here.
+
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+
+    list_display = ["name", "description", "price", "quantity"]
+    ordering = ["name"]
+    list_per_page = 5
+    search_fields = ["name"]
+
+    fieldsets = [
+        (
+            "Наименование",
+            {
+                "classes": ["wide"],
+                "fields": ["name"],
+            },
+        ),
+        (
+            "Подробности",
+            {
+                "description": "Описание товара",
+                "fields": ["description"],
+            },
+        ),
+        (
+            "Цена",
+            {
+                'description': 'Цена товара',
+                "fields": ["price"],
+            },
+        ),
+        (
+            "Количество",
+            {
+                "description": "Количество",
+                'fields': ['quantity']
+            },
+        ),
+    ]
+
+
+@admin.register(Client)
+class Client(admin.ModelAdmin):
+
+    list_display = ["name", "email", "phone", "address"]
+    ordering = ["name"]
+    list_per_page = 5
+    search_fields = ["name"]
+
+    fieldsets = [
+        (
+            "Наименование",
+            {
+                "classes": ["wide"],
+                "fields": ["name"],
+            },
+        ),
+        (
+            "Подробности",
+            {
+                "description": "Email пользователя",
+                "fields": ["email"],
+            },
+        ),
+        (
+            "Цена",
+            {
+                'description': 'Телефон',
+                "fields": ["phone"],
+            },
+        ),
+        (
+            "Количество",
+            {
+                "description": "Адрес",
+                'fields': ['address']
+            },
+        ),
+    ]
+
+
+admin.site.register(Order)
